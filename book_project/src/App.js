9318ebfcef5249fbed29755726cfe2c9587568d4
daniel_lux <daniel.mogilny322@gmail.com>
2024-06-06 01:57:41 +0300

    Color Organizer

--- book_project/src/App.js
+++ book_project/src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
-import StarRating from "./StarRating"
+import { useState } from "react";
+import ColorList from "./ColorList";
+import colorData from "./color_data.json";
 import './App.css';
 
 function App() {
+
+  const [colors, setColors] = useState(colorData);
   return (
     <div className="App">
-      <StarRating/>
+      <ColorList colors={colors} onRemoveColor={id => {
+        const newColors = colors.filter(color => color.id !== id);
+        setColors(newColors);
+      }}
+        onRateColor={(id, rating)=> {
+          const newColors = colors.map(color => color.id === id ? {...color, rating} : color);
+          setColors(newColors);
+        }}/>
     </div>
   );
 }
--- book_project/src/StarRating.jsx
+++ book_project/src/StarRating.jsx
@@ -1,12 +1,25 @@
 import { FaStar } from "react-icons/fa";
 
 
-const createArray = (length) => [...Array(length)];
+const Star = ({selected = false, onSelect = f => f}) => (
 
-const Star = ({selected = false}) => (
-  <FaStar color={selected ? "red" : "grey" }/>
+  <FaStar color={selected ? "red" : "grey" } onClick={onSelect}/>
+
 );
 
-export default function StarRating({ totalStars = 5 }) {
-  return createArray(totalStars).map((n, i) => <Star key={i} />);
+const createArray = (length) => [...Array(length)];
+
+function StarRating({ style = {}, totalStars = 5, selectedStars, onRate = f => f}) {
+
+  return (
+    <div style = {{padding: "5px", ...style}}>
+      {
+        createArray(totalStars).map((n, i) => <Star key={i} selected={selectedStars > i} onSelect={() => onRate(i + 1)}/>)
+      }
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </div>);
 }
+
+export default StarRating;
