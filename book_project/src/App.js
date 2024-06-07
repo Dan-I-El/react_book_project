3dee4a9cf1a27b8784f55e4e008c925f716965a4
daniel_lux <daniel.mogilny322@gmail.com>
2024-06-07 21:31:18 +0300

    color form component

--- book_project/src/App.js
+++ book_project/src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
 import { useState } from "react";
+import ColorForm from "./ColorForm";
 import ColorList from "./ColorList";
 import colorData from "./color_data.json";
+import { v4 } from "uuid";
 import './App.css';
 
 function App() {
@@ -9,6 +10,19 @@ function App() {
   const [colors, setColors] = useState(colorData);
   return (
     <div className="App">
+      <ColorForm
+        onNewColor={(title, color)=> {
+          const newColors = [
+            ...colors,
+            {
+              id: v4(),
+              rating: 0,
+              title,
+              color
+            }
+          ]
+          setColors(newColors);
+        }}/>
       <ColorList colors={colors} onRemoveColor={id => {
         const newColors = colors.filter(color => color.id !== id);
         setColors(newColors);
---
+++ book_project/src/ColorForm.jsx
@@ -0,0 +1,30 @@
+import { useInput } from "./hooks";
+
+export default function AddColorForm({ onNewColor = f=> f}) {
+    const [titleProps, setTitle ] = useInput("");
+    const [colorProps, setColor] = useInput("#000000");
+
+    const submit = e=> {
+        e.preventDefault();
+        onNewColor(titleProps.value, colorProps.value);
+        setTitle("");
+        setColor("");
+    }
+
+    return (
+        <form onSubmit={submit}>
+            <input 
+                {...titleProps}
+                type="text"
+                placeholder="color title..."
+                required
+            />
+            <input 
+                {...colorProps}
+                type="color"
+                required
+            />
+            <button>ADD</button>
+        </form>
+    )
+}
